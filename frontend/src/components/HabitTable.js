b8c62e8289d81f6c5c98414774b7cad39119e42a
Roshan <roshan.bhusal@learningpool.com>
2024-01-03 17:01:13 +0000

    the code is a complete mess oh god

--- frontend/src/components/HabitTable.js
+++ frontend/src/components/HabitTable.js
@@ -1,66 +1,88 @@
-import React, { useState } from 'react'
-import './HabitTable.css'
-
-// Dont forget to put documentation on your code cuz I dont understand my own code LOLLLL
+import React, { useState } from 'react';
+import './HabitTable.css';
 
 function HabitTable() {
-
-  const today = new Date();
-  const todayDate = today.getDate();
-  const daysArray = [];
-
   const [month, setMonth] = useState('');
   const [year, setYear] = useState('');
   const [dateArray, setDateArray] = useState([]);
-  const [addHabit, setAddHabit]=useState('');
+  const [dailyHabits, setDailyHabits] = useState([]);
+  const [addHabit, setAddHabit] = useState('');
+  const [habitpDate, setHabitpDate]=useState([]);
+  const [habitValue, setHabitValue]=useState('')
+  
 
+
+  function addHabits() {
+    if (addHabit.trim() !== '') {
+      const updatedHabits = [...dailyHabits];
+      updatedHabits.push(addHabit);
+      setDailyHabits(updatedHabits);
+      setAddHabit('');
+    }
+  }
 
   function getCorrectDates(month, year) {
     const numDays = new Date(year, month, 0).getDate();
+    const daysArray = [];
 
     for (let i = 0; i < numDays; i++) {
       daysArray[i] = i + 1;
     }
 
     return daysArray;
-  };
-
-  function handleDates(){
-      setDateArray(getCorrectDates(month, year))
   }
 
+  function handleDates() {
+    setDateArray(getCorrectDates(month, year));
+  }
 
+  function habitDetails(index, habit, value) {
+    console.log(index + " " + habit + " " + value);
+  }
+
+  function handleHabitpDate(day, habit){
+
+    const newHabitpDate = {day, habit, value:habitValue}
+    
+    setHabitpDate((preHabitpDate)=>[...preHabitpDate,newHabitpDate ])
+    setHabitValue('');
+    console.log(habitpDate);
+
+  }
 
 
   return (
     <div>
-
       <div className='select-dates'>
-          <input placeholder='month' onChange={(e)=>setMonth(e.target.value)} />
-          <input placeholder='year'onChange={(e)=>setYear(e.target.value)}/>
-          <button onClick={handleDates}>Submit</button>
+        <input placeholder='month' onChange={(e) => setMonth(e.target.value)} />
+        <input placeholder='year' onChange={(e) => setYear(e.target.value)} />
+        <button onClick={handleDates}>Submit</button>
       </div>
 
       <div>
-        <h1>Days</h1>
-        <input placeholder='add habit' onChange={()}/>
-        <div className='days'>
-          {dateArray.map((key, item) => (
-            <div>
-            <input disabled key={key} placeholder={item} />
-            <input disabled placeholder={key} />
 
-            </div>
-
-          ))}
+        <div>
+          <input placeholder='add habit' value={addHabit} onChange={(e) => setAddHabit(e.target.value)} />
+          <button onClick={addHabits}>Add</button>
         </div>
 
+        <div className='days'>
+          <div className='daysCont'><h1>Days</h1>
+            {dailyHabits.map((e) => (<h2 className='headers'>{e}</h2>))}</div>
+          {dateArray.map((day) => (
+            <div key={day} className='habNbtn'>
+              <input disabled placeholder={day} />
+              {dailyHabits.map((habit, index) => (
+                <div >  <input key={index} placeholder={habit} onChange={(e) => setHabitValue(e.target.value)} /> 
+                <button onClick={()=>handleHabitpDate(day,habit)}>submit</button>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
-
-
-      { }
     </div>
-  )
+  );
 }
 
-export default HabitTable
+export default HabitTable;
