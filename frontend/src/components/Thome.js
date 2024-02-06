720172141e45135e4c3cd616f942d718a1670a6b
Roshan <roshan.bhusal@learningpool.com>
2024-02-06 14:24:43 +0000

    testing

---
+++ frontend/src/components/Thome.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Thome() {
+
+
+
+
+
+  return (
+    <div>
+
+        <div>
+                <input placeholder='write a habit name'/>
+                <button>Add Habit</button>
+        </div>
+
+            {<h1>hello</h1>}
+    </div>
+  )
+}
+
+export default Thome
