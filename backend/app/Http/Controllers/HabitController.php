73be518d4fb742ecc5450c7cae4052af4bdf4d70
Roshan <roshan.bhusal@learningpool.com>
2024-02-07 11:34:07 +0000

    added date from backend instead

--- backend/app/Http/Controllers/HabitController.php
+++ backend/app/Http/Controllers/HabitController.php
@@ -8,12 +8,14 @@ use App\Models\Habit;
 use App\Models\HabitLog;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\dd;
 
 class HabitController extends Controller
 {
     public function index()
     {
-        // $habits = Habit::all(); 
+        $habits = Habit::all(); 
         
         return response()->json($habits);
         // Log::info(response()->json($habits));
@@ -23,9 +25,9 @@ class HabitController extends Controller
     public function getUserHabits($userId)
     {
         $user = User::where('id', $userId)->firstOrFail();
-        $habits= $user->habits()
+        $habits= $user->habits()    //recalling the habits function which contains "User has many habits" in User Model
         ->select('id', 'user_id', 'habitName')
-        ->with('logs:id,habit_id,date,completed,created_at,updated_at')
+        ->with('logs:id,habit_id,date,completed,created_at,updated_at') // this Some how calls the logs function in Habit Model
         ->get();
 
         return response()->json($habits);
@@ -33,22 +35,20 @@ class HabitController extends Controller
 
     public function createAndLog(Request $request)
     {
-        // Log::info($request->all()['habit']['email']);
-        // Log::info($request->all()['habit']['habitName']);
-        // Log::info($request->all()['habit']['date']);
-        // Log::info($request->all()['habit']['completed']);
+        
+       $currentDate = Carbon::now()->format('Y-m-d');
 
 
-
+       
         $userEmail = $request->all()['habit']['email'];
-        $user = User::where('email', $userEmail)->firstOrFail();
+        $user = User::where('email', $userEmail)->firstOrFail();    // if I dont use it like this and instead use findadnfail it gives error
 
         $habit = $user->habits()->create([
-            'habitName' => $request->all()['habit']['habitName']
+            'habitName' => $request->all()['habit']['habitName']    
         ]);
 
         $habit->logs()->create([
-            'date' => $request->all()['habit']['date'],
+            'date' => $currentDate,
             'completed' => $request->all()['habit']['completed']
         ]);
 
--- frontend/src/components/Thome.js
+++ frontend/src/components/Thome.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AuthUser from './AuthUser';
 import axios from 'axios';
 
@@ -12,6 +12,7 @@ function Thome() {
 
             const [habit, setHabit]= useState("")
             const [bool, setBool]= useState("")
+            const [getHabs, seGetHabs] =useState([])    //stores the fetched habits from backend
 
 
 
@@ -55,7 +56,19 @@ function Thome() {
 
         }
 
+        useEffect(()=>{
+            fetchHabits();
+        }, []);
 
+        const fetchHabits = ()=>{
+            http.get('/users/2/habits').then(response=>{
+                console.log(response.data);
+                seGetHabs(response.data)
+            })
+            .catch(error =>{
+                console.log("Error fetching data:", error);
+            })
+        }
   return (
     <div>
         
@@ -69,7 +82,32 @@ function Thome() {
 
                 
         </div>
+            <div>
+                <h1> List of habits Habits</h1>
+                {getHabs.map(item => (
+                   <div> 
+                    
+                    <h2 key={item.id}>Habit:{item.habitName}</h2>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                    {item.logs.map(log=>(
+                            <div> 
+                        <li key={log.id}>Completed: {log.completed}}</li>
+                        <input type="checkbox"/>
+                        <li key={log.id}>Completed: {log.date}</li>
+                        
 
+
+                            </div>                        
+                    ))}
+                    
+                   
+                   
+                   
+                    </div>
+                    
+                ))}
+            </div>
           
     </div>
   )
